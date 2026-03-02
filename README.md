5d5f87dfb1fd3f143ddd90ee9029c2ee036bee38
scry1-csv <monoredgoblins60@gmail.com>
2026-03-03 01:45:43 +0900

    Vorzeの変換メソッドを追加

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Scry1ScriptTools
+﻿# Scry1ScriptTools
+
+大人のオモチャ用の連動スクリプト用のツール集です。
+
+- SexToyScriptViewer: スクリプトの内容をグラフ表示。一度に複数のスクリプトを同時に表示できる
+- SexToyScriptConverter: スクリプトを他の形式へ変換する。変換前後の内容をグラフ表示できる
+- 共通機能: 音声や動画ファイルを読み込んで再生することでチャート上に再生位置の目印を表示でき、音声を聞きながらスクリプトの確認が可能
+
+## SexToyScriptViewer 対応スクリプト形式
+
+- funscript
+- U.F.O. SA/A10サイクロンSA用CSV
+- U.F.O. TW用CSV
+- タイムローター用CSV
+
+A10ピストン用CSVは現在非対応です。(今後対応予定)
+
+## SexToyScriptConverter 対応スクリプト形式
+
+現在のところ、funscriptまたはUFOSA/A10サイクロンSA用CSVから、CoyoteScript（[WebSexToysPlayer](https://github.com/scry1-csv/WebSexToysPlayer)でCoyote V3を連動させるためのスクリプト形式）への変換のみに対応しています。
+
+## 使用ライブラリ
+[OxyPlot 2.0.0](https://github.com/oxyplot/oxyplot)
+
+## ライセンス
+MIT License
+
+## バージョン履歴
+
+2026.03.03 最初のリリース
+
+## 不具合報告
+
+リポジトリの Issues 又は [https://twitter.com/scry1\_](https://twitter.com/scry1_) まで
--- SexToyScriptConverter/Converter/VorzeToCoyote.cs
+++ SexToyScriptConverter/Converter/VorzeToCoyote.cs
@@ -1,4 +1,4 @@
-using Core;
+﻿using Core;
 using Core.Script;
 using System.Collections.Immutable;
 
@@ -9,7 +9,10 @@ namespace SexToyScriptConverter.Converter
         public record ConverterData(Func<Vorze_SA, CoyoteScript> Method, string Name);
 
         public static readonly ImmutableArray<ConverterData> Converters = [
-            new(ConvertUp, "上昇")
+            new(ConvertDirectReverseWeak, "逆回転時弱"),
+            new(ConvertReverseStop, "逆回転時一時停止"),
+            new(ConvertUp, "上昇"),
+            new(ConvertDown, "下降"),
             ];
 
         public static CoyoteScript ConvertUp(Vorze_SA vorze)
@@ -20,22 +23,11 @@ namespace SexToyScriptConverter.Converter
             var nonZero = vorze.ScriptData.Where(r => r.Power > 0).ToArray();
             int oldmin = nonZero.Length > 0 ? nonZero.Min(r => r.Power) : 0;
 
-            int newmax = 100;
-            int newmin = 20;
-
-            foreach (var row in origin)
-                if (row.Power != 0)
-                    if (oldmax - oldmin != 0)
-                        row.Power = (int)CommonUtil.Normalize(row.Power, oldmax, oldmin, newmax, newmin);
-                    else
-                        row.Power = newmin;
-
             int freq = 10;
 
-            var result = new List<CoyoteScript.ScriptRow>
-            {
-                new() { InternalTime = 0, Frequency = freq, Strength = 0 }
-            };
+            List<CoyoteScript.ScriptRow> result = [];
+            if (vorze.ScriptData.Count > 0 && vorze.ScriptData[0].Power != 0)
+                result.Add(new() { InternalTime = 0, Frequency = freq, Strength = 0 });
 
             var prev = origin[0];
             foreach (var row in vorze.ScriptData)
@@ -76,6 +68,125 @@ namespace SexToyScriptConverter.Converter
                 }
 
                 prev = row;
+            }
+
+            return new CoyoteScript { ScriptData = result, FileName = "", FilePath = "" };
+        }
+
+        public static CoyoteScript ConvertDown(Vorze_SA vorze)
+        {
+            var origin = vorze.ScriptData.ToArray();
+
+            int oldmax = vorze.ScriptData.Max(r => r.Power);
+            var nonZero = vorze.ScriptData.Where(r => r.Power > 0).ToArray();
+            int oldmin = nonZero.Length > 0 ? nonZero.Min(r => r.Power) : 0;
+
+            int freq = 10;
+
+            List<CoyoteScript.ScriptRow> result = [];
+            if (vorze.ScriptData.Count > 0 && vorze.ScriptData[0].Power != 0)
+                result.Add(new() { InternalTime = 0, Frequency = freq, Strength = 0 });
+
+            var prev = origin[0];
+            foreach (var row in vorze.ScriptData)
+            {
+                if (prev.Power == 0)
+                {
+                    result.Add(new() { InternalTime = (int)prev.Milliseconds, Frequency = freq, Strength = 0 });
+                    prev = row;
+                    continue;
+                }
+
+
+                int targetStr = (row.Direction == prev.Direction && prev.Power != 0)
+                    ? row.Power
+                    : Math.Max(row.Power - 40, 0);
+
+                int cursor = (int)prev.Milliseconds;
+
+                double strength = prev.Power;
+                double stepCount = (row.Milliseconds - prev.Milliseconds) / 25.0;
+
+                if (stepCount <= 0)
+                {
+                    prev = row;
+                    continue;
+                }
+
+                // 前のパワーから目標強度へ向かう 1 ステップあたりの変化量
+                double step = (prev.Power - targetStr) / stepCount;
+                while (cursor < row.Milliseconds)
+                {
+                    result.Add(new CoyoteScript.ScriptRow { InternalTime = cursor, Frequency = freq, Strength = (int)strength });
+                    cursor += 25;
+                    strength -= step;
+
+                    if (strength > 100)
+                        throw new Exception("strength > 100");
+                }
+
+                prev = row;
+            }
+
+            return new CoyoteScript { ScriptData = result, FileName = "", FilePath = "" };
+        }
+
+        public static CoyoteScript ConvertReverseStop(Vorze_SA vorze)
+        {
+            const int freq = 10;
+
+            var result = new List<CoyoteScript.ScriptRow>();
+
+            if (vorze.ScriptData.Count > 0 && vorze.ScriptData[0].Power != 0)
+                result.Add(new() { InternalTime = 0, Frequency = freq, Strength = 0 });
+
+            var prev = vorze.ScriptData[0];
+            foreach (var row in vorze.ScriptData)
+            {
+                double diff = row.Milliseconds - prev.Milliseconds;
+
+                if (row.Direction != prev.Direction && row.Power != 0)
+                {
+                    double delay = diff < 400 ? diff / 2 : 200;
+                    result.Add(new() { InternalTime = (int)(row.Milliseconds - delay), Frequency = freq, Strength = 0 });
+                }
+
+                result.Add(new() { InternalTime = (int)row.Milliseconds, Frequency = freq, Strength = row.Power });
+
+                prev = row;
+            }
+
+            return new CoyoteScript { ScriptData = result, FileName = "", FilePath = "" };
+        }
+
+        public static CoyoteScript ConvertDirectReverseWeak(Vorze_SA vorze)
+        {
+            const int freqUp = 10;
+            const int freqDown = 20;
+            const double factorDown = 0.5;
+
+            var result = new List<CoyoteScript.ScriptRow>();
+
+            if (vorze.ScriptData.Count > 0 && vorze.ScriptData[0].Power != 0)
+                result.Add(new() { InternalTime = 0, Frequency = freqUp, Strength = 0 });
+
+            foreach (var row in vorze.ScriptData)
+            {
+                int freq;
+                int power;
+
+                if (row.Direction)
+                {
+                    freq = freqUp;
+                    power = row.Power;
+                }
+                else
+                {
+                    freq = freqDown;
+                    power = (int)(row.Power * factorDown);
+                }
+
+                result.Add(new() { InternalTime = (int)row.Milliseconds, Frequency = freq, Strength = power });
             }
 
             return new CoyoteScript { ScriptData = result, FileName = "", FilePath = "" };
