d1d789853d2f8413c9c363eb9f7d187c2f9fbc04
scry1-csv <monoredgoblins60@gmail.com>
2026-03-04 02:29:35 +0900

    publishを追加

---
+++ publish.bat
@@ -0,0 +1,2 @@
+dotnet publish .\SexToyScriptConverter\SexToyScriptConverter.csproj /p:PublishProfile=FolderProfile
+dotnet publish .\SexToyScriptViewer\SexToyScriptViewer.csproj /p:PublishProfile=FolderProfile
